5122b9049176b71e6fafd8ce30952f7ab36666fd
Logan Emerson <logemerson@gmail.com>
2019-07-01 20:23:54 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function BoardMember(name, homeState, training) {
+  this.veto = function() {
+    return `No, I must disagree`
+  }
+
+  this.approve = function() {
+    return `You can do that!`
+  }
+
+  this.doChjarity = function() {
+    return `I like to help people.`
+  }
+
+  this.releasePressStatement() {
+    `Hi, my name is ${name}. I am from ${homeState}, and I was trained in ${homeState}.`
+  }
+}
